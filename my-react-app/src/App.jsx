bf1001fb0e88ec7468586b935e4baf1feb4b1334
Oblack <ug2528149@ines.ac.rw>
2026-09-17 19:42:39 +0200

    My Website

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,40 +1,8 @@
-// function App() {
-//     return (
-//         <main>
-//             <img src="/react-logo.png" width={40} />
-//             <h1>Fun facts about React</h1>
-//             <ul>
-//                 <li>Was created in 2023</li>
-//                 <li>Was created by Facebook</li>
-//                 <li>React is a JavaScript library</li>
-//             </ul>
-//         </main>
-//     )
-// }
+import Header from './Header.jsx'
 
-// export default App
-
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
-
-root.render(
-    <TemporaryName />
-)
-
-function TemporaryName() {
-    return (
-        <main>
-            <img src="react-logo.png" width="40px" alt="React logo" />
-            <h1>Fun facts about React!</h1>
-            <ul>
-                <li>Was first release in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 200K stars on GitHub</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </main>
-    )
+function App(){
+  return(
+    <Header/>
+  )
 }
-
 export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <header>
+           <h1>My Website</h1>
+           <nav>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Services</li>
+              </ul>
+           </nav>
+        </header>
+    )
+}
+export default Header
