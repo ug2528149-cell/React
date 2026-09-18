a80a91c9ab1d04bf7910031c978465ed81135429
Oblack <ug2528149@ines.ac.rw>
2026-09-18 06:51:26 +0200

    Using External method(here you may face a challenge of repeated class name)

---
+++ my-Button-app/src/App.jsx
@@ -0,0 +1,16 @@
+//HHOW TO STYLE REACT COMPONENTS WITH CSS
+//---------------------------------------
+//(not including external frameworks or preprocessors)
+
+//1.EXTERNAL
+//2.MODULES
+//3.INLINE
+
+import Button from './Button.jsx'
+
+function App(){
+  return(
+    <Button/>
+  )
+}
+export default App
