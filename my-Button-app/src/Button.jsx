6c5ea632a0da1aa744de88b9ac2ea381cdceba7b
Oblack <ug2528149@ines.ac.rw>
2026-09-18 07:34:59 +0200

    INLINE(it prevents global conflict)

---
+++ my-Button-app/src/Button.jsx
@@ -0,0 +1,16 @@
+
+function Button(){
+    const styles ={
+  backgroundColor: "rgb(104, 104, 146)",
+  color: "white",
+  border: "none",
+  borderRadius: "5px",
+  padding: "10px 25px",
+  cursor: "pointer",
+    }
+
+    return(
+        <button style={styles}>Click me</button>
+    )
+}
+export default Button
