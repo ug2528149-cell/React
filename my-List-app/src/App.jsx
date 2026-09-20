7a3a35aec1e8a69ea1535f79f543bd3c0a7c24ef
Oblack <ug2528149@ines.ac.rw>
2026-09-20 18:30:39 +0200

    Create a list of items and arrange in a size of calories

--- my-List-app/src/App.jsx
+++ my-List-app/src/App.jsx
@@ -2,10 +2,27 @@
 import List from './List.jsx'
 
 function App(){
-  return(
-    <>
-      <List/>
-    </>
-  )
+
+const fruits =[{id: 1, name:'apple',calories:95},
+              {id: 2, name:'pineapple',calories:45},
+              {id: 3, name:'guava',calories:105},
+              {id: 4, name:'oranges',calories:159},
+              {id: 5, name:'banana',calories:37},
+              {id: 6, name:'coconut',calories:10}];
+
+ const vegetables =[{id: 1, name:'apple',calories:110},
+              {id: 2, name:'pineapple',calories:15},
+              {id: 3, name:'guava',calories:25},
+              {id: 4, name:'oranges',calories:63},
+              {id: 5, name:'banana',calories:50},
+              {id: 6, name:'coconut',calories:87}]; 
+              
+          return(
+            <>
+              <List items ={fruits} category="Fruits"/>
+              <List items ={vegetables} category="Vegetables"/>
+            </>
+          )
 }
+
 export default App;
--- my-List-app/src/List.jsx
+++ my-List-app/src/List.jsx
@@ -1,22 +1,36 @@
 
 
-function List(){
+// function List(){
 
-    const fruits =[{id: 1, name:'apple',calories:95},
-                  {id: 2, name:'pineapple',calories:45},
-                  {id: 3, name:'guava',calories:105},
-                  {id: 4, name:'oranges',calories:159},
-                  {id: 5, name:'banana',calories:37},
-                  {id: 6, name:'coconut',calories:10}]
+//     const fruits =[{id: 1, name:'apple',calories:95},
+//                   {id: 2, name:'pineapple',calories:45},
+//                   {id: 3, name:'guava',calories:105},
+//                   {id: 4, name:'oranges',calories:159},
+//                   {id: 5, name:'banana',calories:37},
+//                   {id: 6, name:'coconut',calories:10}]
 
-        //fruits.sort((a,b) =>a.name.localCompare(b.name)); //ALPHABETICAL
-        //fruits.sort((a,b) =>b.name.localCompare(a.name)); //REVERSE ALPHABETICAL
+//         //fruits.sort((a,b) =>a.name.localCompare(b.name)); //ALPHABETICAL
+//         //fruits.sort((a,b) =>b.name.localCompare(a.name)); //REVERSE ALPHABETICAL
 
-        fruits.sort((a,b) =>a.calories -b.calories); //NUMERIC
+//         fruits.sort((a,b) =>a.calories -b.calories); //NUMERIC
 
-        const listItems = fruits.map(fruit =><li key={fruit.id}>{fruit.name}&nbsp;<b>{fruit.calories}</b></li>)
+//         const listItems = fruits.map(fruit =><li key={fruit.id}>{fruit.name}&nbsp;<b>{fruit.calories}</b></li>)
 
 
-    return(<ol>{listItems}</ol>)
+//     return(<ol>{listItems}</ol>)
+// }
+// export default List
+
+function List(props){
+
+    const category = props.category;
+    const itemList = props.items;
+
+    const listItems = itemList.map( item => <li key={item.id}>{item.name}: &nbsp; <b>{item.calories}</b></li>)
+
+    return <>
+    <h3 className="list-category">{category}</h3>
+    <ol className="list-items">{listItems}</ol>
+    </>
 }
 export default List
