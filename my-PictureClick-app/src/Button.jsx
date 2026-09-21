990b4a23ee4b1e7f331758725ef0442822e83ca1
Oblack <ug2528149@ines.ac.rw>
2026-09-21 08:25:56 +0200

    Event listeners

--- my-PictureClick-app/src/Button.jsx
+++ my-PictureClick-app/src/Button.jsx
@@ -2,18 +2,8 @@
 
 function Button() {
 
-    let count = 0;
-
-    const handleClick = (name) => {
-
-        if (count < 3) {
-            count++;
-            console.log(`${name} you clicked me ${count} time/s`)
-        } else {
-            console.log(`${name} stop clicking me`)
-        }
-    }
-        return (<button onClick={() =>handleClick('Bro')}>Click me</button>)
+    const handleClick = (e) =>e.target.textContent = "Ouch😉"
+        return (<button onClick={(e) =>handleClick(e)}>Click me</button>)
 
     }
 
