3c66f4cd52d51a1c41c042dacad31cfb71990224
Oblack <ug2528149@ines.ac.rw>
2026-09-21 08:51:29 +0200

    Click events in React passing also the events

--- my-PictureClick-app/src/ProfilePicture.jsx
+++ my-PictureClick-app/src/ProfilePicture.jsx
@@ -1,6 +1,11 @@
 
 function ProfilePicture(){
+   
+    const imageUrl = './src/assets/aaa.PNG'
 
+   const handleClick =(e) =>e.target.style.display=none;
+
+    return(<img onClick={(e)=>handleClick(e)}src={imageUrl}></img>)
 }
 
 export default ProfilePicture
